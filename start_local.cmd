20067e21636e4bb749dbe8594ad6cfd04bf1aade
Alexey Volkov <alexey.volkov@ark-kun.com>
2025-12-02 01:14:27 -0800

    chore: Scripts - start_local - Using fastapi dev to avoid the broken http://0.0.0.0:8000 URL in the log
    The app does not work (browser disables the crypto package) when accessed via http://0.0.0.0:8000 (but http://127.0.0.1:8000 or https://* work) .
    We can either use `fastapi run --host 127.0.0.1` or `fastapi dev` to make the correct URL show up in the log.

--- start_local.cmd
+++ start_local.cmd
@@ -7,6 +7,8 @@ set "CLOUD_PIPELINES_BACKEND_DATA_DIR=%CD%\data"
 REM `cd $(dirname) $0`
 cd /d "%~dp0"
 
-call uv run --frozen fastapi run start_local.py %*
+REM ! The app does not work when accessed via http://0.0.0.0:8000 (but http://127.0.0.1:8000 or https://* work).
+REM We can either use `fastapi run --host 127.0.0.1` or `fastapi dev` to make the correct URL show up in the log.
+call uv run --frozen fastapi dev start_local.py %*
 
 exit /b %ERRORLEVEL%
--- start_local.sh
+++ start_local.sh
@@ -5,4 +5,6 @@ set -e -o pipefail -o nounset
 current_path=`pwd`
 cd $(dirname $0)
 
-CLOUD_PIPELINES_BACKEND_DATA_DIR="${current_path}/data" uv run --frozen fastapi run start_local.py "$@"
+# ! The app does not work when accessed via http://0.0.0.0:8000 (but http://127.0.0.1:8000 or https://* work).
+# We can either use `fastapi run --host 127.0.0.1` or `fastapi dev` to make the correct URL show up in the log.
+CLOUD_PIPELINES_BACKEND_DATA_DIR="${current_path}/data" uv run --frozen fastapi dev start_local.py "$@"
